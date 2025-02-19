65ebba227b17152ab0f5e37c6c2b65381b267229
nataliecastonguay3 <ngcastonguay@gmail.com>
2025-02-19 11:51:35 -0500

    Summary statistics and charts for quantitative variables

--- Draft1.R
+++ Draft1.R
@@ -1,4 +1,42 @@
 ## Test file for midterm project
+library(ggplot2)
 
 movies = read.csv('IMDB_data_Winter_2025.csv')
+attach(movies)
 View(movies)
+
+summary(movie_budget)
+hist(movie_budget)
+boxplot(movie_budget)
+plot = ggplot(movies,aes(y=imdb_score,x=movie_budget))
+plot + geom_point()
+
+summary(release_day)
+hist(release_day)
+boxplot(release_day)
+plot = ggplot(movies,aes(y=imdb_score,x=release_day))
+plot + geom_point()
+
+summary(release_year)
+hist(release_year)
+boxplot(release_year)
+plot = ggplot(movies,aes(y=imdb_score,x=release_year))
+plot + geom_point()
+
+summary(duration)
+hist(duration)
+boxplot(duration)
+plot = ggplot(movies,aes(y=imdb_score,x=duration))
+plot + geom_point()
+
+summary(nb_news_articles)
+hist(nb_news_articles)
+boxplot(nb_news_articles)
+plot = ggplot(movies,aes(y=imdb_score,x=nb_news_articles))
+plot + geom_point()
+
+
+
+
+
+
