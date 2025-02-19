9e05c2de1250665f5b6827bdaec4742125edfcc3
nataliecastonguay3 <ngcastonguay@gmail.com>
2025-02-19 12:26:22 -0500

    Adding Qualitative Variables charts

--- Draft1.R
+++ Draft1.R
@@ -5,33 +5,24 @@ movies = read.csv('IMDB_data_Winter_2025.csv')
 attach(movies)
 View(movies)
 
-summary(movie_budget)
-hist(movie_budget)
-boxplot(movie_budget)
-plot = ggplot(movies,aes(y=imdb_score,x=movie_budget))
+qualitatives = c("release_month", "language", "country", "maturity_rating", 
+                "distributor", "director", "colour_film", "cinematographer", 
+                 "production_company")
+
+for (var in qualitatives) {
+  movies[[var]] <- as.factor(movies[[var]])
+}
+
+quantvar = movie_budget 
+summary(quantvar)
+hist(quantvar)
+boxplot(quantvar)
+plot = ggplot(movies,aes(y=imdb_score,x=quantvar))
 plot + geom_point()
 
-summary(release_day)
-hist(release_day)
-boxplot(release_day)
-plot = ggplot(movies,aes(y=imdb_score,x=release_day))
-plot + geom_point()
+qualvar = colour_film
+ggplot(movies, aes(x = qualvar)) + geom_bar() + theme(axis.text.x = element_text(angle = 90))
+ggplot(movies, aes(x = qualvar, y = imdb_score)) + geom_boxplot() + theme(axis.text.x = element_text(angle=90))
 
-summary(release_year)
-hist(release_year)
-boxplot(release_year)
-plot = ggplot(movies,aes(y=imdb_score,x=release_year))
-plot + geom_point()
 
-summary(duration)
-hist(duration)
-boxplot(duration)
-plot = ggplot(movies,aes(y=imdb_score,x=duration))
-plot + geom_point()
-
-summary(nb_news_articles)
-hist(nb_news_articles)
-boxplot(nb_news_articles)
-plot = ggplot(movies,aes(y=imdb_score,x=nb_news_articles))
-plot + geom_point()
 
